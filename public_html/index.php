08b689a33b4133b07ee57ad8fdceb7230a92ee32
mside <michaelside0@gmail.com>
2017-05-29 12:20:14 -0700

    initial

---
+++ public_html/index.php
@@ -0,0 +1,28 @@
+
+<!DOCTYPE html>
+<!--[if IE 8]> 				 <html class="no-js lt-ie9" lang="en" > <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en" > <!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>VANARTS STUDENT MOCK PROJECT</title>
+	<meta name="description" content="This is a student exercise website for the Vancouver Institute of Media Arts (www.vanarts.com)">
+	<link rel="icon" type="image/png"  href="favicon.png">
+<!-- CSS -->
+	<link href="css/foundation/foundation.css" rel="stylesheet" type="text/css">
+  <link href="css/foundation/normalize.css" rel="stylesheet" type="text/css">
+	<link href="css/main.css" media="screen, projector, print" rel="stylesheet" type="text/css">
+<!-- modernizr -->
+  <script src="js/vendor/custom.modernizr.js"></script>
+
+</head>
+<body>
+	<header class="header-wrapper">
+
+	</header>
+
+
+
+	<?php include "partials/footer.php"; ?>
+</body>
+</html>
